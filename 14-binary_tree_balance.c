dcfa077f40325e6aac2236ecdd230aca5d44eeb6
Mosesomo <moseswamboga4@gmail.com>
2023-11-02 01:17:09 -0700

    measure balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,10 +9,15 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree)
-		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	int bf, lh = 0, rh = 0;
 
-	return (0);
+	if (tree == NULL)
+		return (0);
+	lh = binary_tree_height(tree->left);
+	rh = binary_tree_height(tree->right);
+	bf = lh - rh;
+
+	return (bf);
 }
 
 /**
@@ -23,30 +28,24 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t lh = 0, rh = 0;
-
 	if (tree == NULL)
 		return (0);
+
+	size_t lh = 0, rh = 0;
 
 	if (tree->left)
 	{
 		lh = 1 + binary_tree_height(tree->left);
 	}
 	else
-		lh = 0;
+		return (1);
 	if (tree->right)
 	{
 		rh = 1 + binary_tree_height(tree->right);
 	}
 	else
-		rh = 0;
+		return (1);
 
-	if (lh > rh)
-	{
-		return (lh);
-	}
-	else
-	{
-		return (rh);
+	return (lh > rh ? lh : rh);
 	}
 }
