e85e2c0f56a162ab362e5a396d84a1ee77e33aec
Mosesomo <moseswamboga4@gmail.com>
2023-11-02 08:48:28 -0700

    debug

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,22 +8,21 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t lh = 0, rh = 0;
+	size_t rh = 0, lh = 0;
 
-        if (tree == NULL)
-                return (0);
-
-        if (tree->left)
-                lh = 1 + binary_tree_height(tree->left);
-        else
-                return (1);
-
-        if (tree->right)
-                rh = 1 + binary_tree_height(tree->right);
-        else
-                return (1);
-
-        return ((lh > rh) ? lh : rh);
+	if (tree == NULL)
+	{
+		return  (0);
+	}
+	else
+	{
+		if (tree)
+		{
+			lh = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+			rh = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		}
+		return ((lh > rh) ? lh : rh);
+	}
 }
 
 /**
