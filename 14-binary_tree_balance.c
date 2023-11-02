ac65add93b91396e91fb07edda7a319f30b7827f
Mosesomo <moseswamboga4@gmail.com>
2023-11-02 08:29:56 -0700

    correction

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,32 @@
 #include "binary_trees.h"
 
 /**
+ *binary_tree_height - checks the height of the tree
+ *@tree: pointer to the root node of the tree
+ *Return: the height of the tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+        size_t lh = 0, rh = 0;
+
+        if (tree == NULL)
+                return (0);
+
+        if (tree->left)
+                lh = 1 + binary_tree_height(tree->left);
+        else
+                return (1);
+
+        if (tree->right)
+                rh = 1 + binary_tree_height(tree->right);
+        else
+                return (1);
+
+        return ((lh > rh) ? lh : rh);
+}
+
+/**
  *binary_tree_balance - Measures the tree balnce
  *@tree: pointer to the root node of the tree to measure
  *
@@ -15,28 +41,3 @@ int binary_tree_balance(const binary_tree_t *tree)
 	return (0);
 }
 
-/**
- *binary_tree_height - Measures the height of a tree
- *@tree: pointer to the roort node of the tree to measurie
- *Return: height of the tree
- */
-
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t lh = 0, rh = 0;
-
-	if (tree == NULL)
-		return (0);
-
-	if (tree->left)
-		lh = 1 + binary_tree_height(tree->left);
-	else
-		return (1);
-
-	if (tree->right)
-		rh = 1 + binary_tree_height(tree->right);
-	else
-		return (1);
-
-	return ((lh > rh) ? lh : rh);
-}
