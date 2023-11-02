e1a10e71b4af7087da5c3799c755ecd7252bbbe5
Mosesomo <moseswamboga4@gmail.com>
2023-11-02 02:45:44 -0700

    perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -44,7 +44,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree->left && tree->right)
 	{
 		height = binary_tree_height(tree);
-		return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right) && height > 0);
+		return (binary_tree_is_perfect(tree->left) &&
+				binary_tree_is_perfect(tree->right) && height > 0);
 	}
 	return (0);
 }
