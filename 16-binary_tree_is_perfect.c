4cba4e50fb79db4e500dbb1eab5bae545db3cfb5
Mosesomo <moseswamboga4@gmail.com>
2023-11-02 09:09:54 -0700

    correction

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -11,19 +11,55 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t lh = 0, rh = 0;
 
 	if (tree == NULL)
+	{
+		return  (0);
+	}
+	else
+	{
+		if (tree)
+		{
+			lh = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+			rh = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		}
+		return ((lh > rh) ? lh : rh);
+	}
+}
+
+/**
+ *binary_tree_size - measure the size of the tree
+ *@tree: pointer to the root node of the tree to measure
+ *
+ *Return: size of the tree
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0;
+
+	if (tree == NULL)
 		return (0);
 
-	if (tree->left)
-		lh = 1 + binary_tree_height(tree->left);
-	else
-		return (1);
+	size = 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
 
-	if (tree->right)
-		rh = 1 + binary_tree_height(tree->right);
-	else
-		return (1);
+	return (size);
+}
 
-	return ((lh > rh) ? lh : rh);
+/**
+ *_pow_recursion - returns the value of x raised to the power of y
+ *@x: the value to exponentiate
+ *@y: the power to raise x to
+ *Return: x to the power of y, or -1 if y is negative
+ */
+
+int _pow_recursive(int i, int j)
+{
+	if (j < 0)
+		return (-1);
+	if (j == 0)
+		return (1);
+	else
+		return (i * _pow_recursive(i, j - 1));
+
 }
 
 /**
@@ -35,17 +71,16 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int height;
+	size_t height = 0, node = 0, i = 0;
 
 	if (tree == NULL)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	if (tree->left && tree->right)
-	{
-		height = binary_tree_height(tree);
-		return (binary_tree_is_perfect(tree->left) &&
-				binary_tree_is_perfect(tree->right) && height > 0);
-	}
-	return (0);
+	
+	height = binary_tree_height(tree);
+	node = binary_tree_size(tree);
+	i = (size_t)_pow_recursive(2, height + 1);
+
+	return (i - 1 == node);
 }
